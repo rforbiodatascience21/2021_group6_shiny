1784ed0ecf1862a604371fb2c7147bf80c7e195f
chrhjoh <s202770@student.dtu.dk>
2021-04-12 09:36:17 +0000

    Fix: Complement dna : No longer displays a complement strand when app launches

--- R/99_functions.R
+++ R/99_functions.R
@@ -1,7 +1,10 @@
 complement <- function(dna){
   dna <- toupper(dna)
+  if (dna == 'WRITE DNA:') {return('') }
+  else {
   lookup <- c("A" = "T", "T" = "A", "G" = "C", "C" = "G") # named vector, think key/value
   dna_split <- strsplit(dna, "")[[1]]
   dna_complement <- paste0(lookup[dna_split], collapse = "")
   return(dna_complement)
+  }
 }
--- our_first_app/ui.R
+++ our_first_app/ui.R
@@ -6,17 +6,19 @@ shinyUI(fluidPage(
   titlePanel("Gravier Data"), #Application title
   theme = shinytheme("cerulean"),
   sidebarLayout(
-  sidebarPanel(
-  selectInput("gene", "Select Gene:",
+    sidebarPanel(
+      selectInput("gene", "Select Gene:",
               c("gene1" = 'g7F07',
                 "gene2" = 'g2E09',
                 "gene3" = 'g1A08')),
-  textInput("dna", "Complement DNA","WriteDNA:")
-  )
-  ,mainPanel(
-  plotOutput("boxplot"),
-  h3('Complemented DNA'),
-  textOutput(outputId = 'complement',)
-  )
-  )))
+      textInput("dna", "Complement DNA","Write DNA:")
+                ),
+  
+    mainPanel(
+      plotOutput("boxplot"),
+      h3('Complemented DNA'),
+      textOutput(outputId = 'complement')
+      )
+  ))
+)
 
