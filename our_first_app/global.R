4f4553600901e41afd565e9f007c34da246be747
chrhjoh <s202770@student.dtu.dk>
2021-04-12 10:05:50 +0000

    Changed data to now be loaded from CSV instead of Rdata

--- our_first_app/global.R
+++ our_first_app/global.R
@@ -3,10 +3,5 @@ library("tidyverse")
 
 
 
-load('../data/gravier.RData')
-set.seed(676571)
-cancer_data=mutate(as_tibble(pluck(gravier,"x")),y=pluck(gravier,"y"),pt_id=1:length(pluck(gravier, "y")),age=round(rnorm(length(pluck(gravier,"y")),mean=55,sd=10),1))
-cancer_data=rename(cancer_data,event_label=y)
-cancer_data$age_group=cut(cancer_data$age,breaks=seq(10,100,by=10))
-
+cancer_data = read_csv('../data/cancer_data.csv')
 select(slice(cancer_data,1:8),pt_id,age,age_group,event_label,1:5)
