c447de6159d8709d42a9066a5d5bfb629d161b16
Anna-Jorgensen <aejorge01@gmail.com>
2021-04-12 08:22:29 +0000

    added data

---
+++ our_first_app/global.R
@@ -0,0 +1,13 @@
+install.packages("tidyverse")
+install.packages("remotes")
+remotes::install_github("ramhiser/datamicroarray")
+library("tidyverse")
+library("datamicroarray")
+
+data('gravier', package = 'datamicroarray')
+set.seed(676571)
+cancer_data=mutate(as_tibble(pluck(gravier,"x")),y=pluck(gravier,"y"),pt_id=1:length(pluck(gravier, "y")),age=round(rnorm(length(pluck(gravier,"y")),mean=55,sd=10),1))
+cancer_data=rename(cancer_data,event_label=y)
+cancer_data$age_group=cut(cancer_data$age,breaks=seq(10,100,by=10))
+
+select(slice(cancer_data,1:8),pt_id,age,age_group,event_label,1:5)
