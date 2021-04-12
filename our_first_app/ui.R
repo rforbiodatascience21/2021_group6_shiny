3efd039d33c3b64f0fb390625ca4a60663b8d5da
Anna-Jorgensen <aejorge01@gmail.com>
2021-04-12 09:08:57 +0000

    Selects data now

--- our_first_app/ui.R
+++ our_first_app/ui.R
@@ -2,12 +2,21 @@ library(shiny)
 library(shinythemes)
 
 
-ui <- fluidPage(
+shinyUI(fluidPage(
   theme = shinytheme("cerulean"),
+  
+  # Application title
+  titlePanel("Gravier Data"),
+  
   selectInput("gene", "Select Gene:",
-              c("gene1" = 'g7F07',
-                "gene2" = 'g2E09',
-                "gene3" = 'g1A08')),
-  plotOutput("boxplot")
+              c("gene1" = "g7F07",
+                "gene2" = "g2E09",
+                "gene3" = "g1A08")
+              ),
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("boxplot")
+  
 )
-
+))
